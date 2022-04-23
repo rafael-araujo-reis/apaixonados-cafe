5a86c706bad3e8dd877f88c55451286a871b6aae
Rafael Reis <rafa.araujoreis@gmail.com>
2022-04-23 03:00:05 -0300

    add page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import Banner from "../../components/Banner";
+import Cards from "../../components/Cards";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Banner />
+
+      <Cards />
+
+      <Footer />
+    </>
+  );
+}
